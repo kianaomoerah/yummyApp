1ea6619af688f490545e990a0d1b274be3a494ae
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-24 17:22:16 -0400

    completed api call to retrieve random popular recipes

--- src/components/PopularRecipes.js
+++ src/components/PopularRecipes.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from "react";
+
 const PopularRecipes = () => {
 
-    return (
-        <>
-            <h1>Popular Recipies</h1>
-        </>
-    )
+    const [popular, setPopular] = useState([])
+
+    useEffect(() => {
+
+        getPopular();
+
+    },[] )
+
+    const getPopular = async () => {
+
+        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+        const data = await api.json();
+        setPopular(data.recipes)
+        // console.log(data.recipes)
+
+    };
+
+    return <ul>
+        {popular.map((recipe) => {
+            return (
+                <li key={recipe.id}>
+                    <p>{recipe.title}</p>
+                </li>
+            )
+        })}    
+    </ul>
 
 }
 
