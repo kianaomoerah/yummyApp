0425e0b232b1ae97389166b4ec6bae1c2d246db2
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-24 21:25:39 -0400

    card and gardient styling

--- src/components/PopularRecipes.js
+++ src/components/PopularRecipes.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components"
 import { Splide, SplideSlide } from "@splidejs/react-splide"
-// import '@splidejs/splide/dist/css/splide.min.css';
-import '@splidejs/react-splide/css';
+import '@splidejs/splide/dist/css/splide.min.css';
+// import '@splidejs/react-splide/css';
 
 const PopularRecipes = () => {
 
@@ -24,36 +24,30 @@ const PopularRecipes = () => {
     };
 
     return ( 
-        <List>
-
+        <div>
             <Wrapper>
-
                 <h3>Popular Picks</h3>
-
-            <Splide options={{
-                perPage:4,
-                arrows: false,
-                pagination: false,
-                drag: 'free',
-                gap: '5rem',
-            }}>
-
-            {popular.map((recipe) => {
-                return (
-
-                    <SplideSlide key={recipe.id}>
-                        <Card>
-                            <p>{recipe.title}</p>
-                            <img src={recipe.image} alt={recipe.title}/>
-                        </Card>
-                    </SplideSlide>
-                );
-            })} 
-
-            </Splide>
-
+                <Splide options={{
+                    perPage:4,
+                    arrows: false,
+                    pagination: false,
+                    drag: 'free',
+                    gap: '5rem',
+                }}>
+                    {popular.map((recipe) => {
+                        return (
+                            <SplideSlide key={recipe.id}>
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img src={recipe.image} alt={recipe.title}/>
+                                    <Gradient />
+                                </Card>
+                            </SplideSlide>
+                        );
+                    })} 
+                </Splide>
             </Wrapper>  
-    </List>
+        </div>
 
     );
     // return <ul>
@@ -76,9 +70,9 @@ const PopularRecipes = () => {
 
 }
 
-const List = styled.ul`
-    list-style: none;
-`
+// const List = styled.ul`
+//     list-style: none;
+// `
 
 const Wrapper = styled.div`
     margin: 4rem 0rem;
@@ -89,11 +83,41 @@ const Card = styled.div`
     min-height: 25rem;
     border-radius: 2rem;
     overflow: hidden;
+    position: relative;
 
-    img{
+    img {
         border-radius: 2rem;
-
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
     }
+
+    p {
+        position: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transform: translate(-50%, 0%);
+        color: white;
+        width: 100%;
+        text-align: center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute; 
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
 `
 
 export default PopularRecipes
