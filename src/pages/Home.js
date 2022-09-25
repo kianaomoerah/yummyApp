ad133785a922f7846f43b841560c8289a3a43867
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-25 17:20:43 -0400

    added animation using motion from framer motion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
-import Veggie from "../components/Veggie"
-import PopularRecipies from "../components/PopularRecipes"
+import Veggie from "../components/Veggie";
+import PopularRecipies from "../components/PopularRecipes";
+import { motion } from "framer-motion";
 
 const Home = () => {
 
     return (
-        <>
+        <motion.div
+            animate={{opacity: 1}}
+            initial={{opacity: 0}}
+            exit={{opactiy: 0}}
+            transition={{duration: 0.7}}
+        >
             <Veggie />
             <PopularRecipies />
-        </>
+        </motion.div>
     )
 
 }
