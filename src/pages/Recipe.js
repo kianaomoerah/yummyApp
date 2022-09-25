490c3c02b55d4a0d8ca593856623aafa054036cd
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-09-25 16:48:30 -0400

    added functionality to add active class to active button altering css and activetab state

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -7,6 +7,7 @@ const Recipe = () => {
     let params = useParams();
 
     const [ details, setDetails ] = useState({});
+    const [ activeTab, setActiveTab ] = useState("instructions");
 
     const fetchDetails = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
@@ -25,8 +26,8 @@ const Recipe = () => {
                 <img src={details.image} alt={details.title}/>
             </div>
             <Info>
-                <Button>Instructions</Button>
-                <Button>Ingredients</Button>
+                <Button className={activeTab === "instructions" ? "active" : ""}onClick={()=> setActiveTab("instructions")}>Instructions</Button>
+                <Button className={activeTab === "ingredients" ? "active" : ""} onClick={()=> setActiveTab("ingredients")}>Ingredients</Button>
             </Info>
         </DetailWrapper>
     )
